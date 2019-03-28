cf719279f8e6f3636ea4df11b9216e46ff8df61f
Tommie <tommiealcorn@gmail.com>
2019-03-28 07:51:20 -0600

    data structures

--- js/app.js
+++ js/app.js
@@ -6,3 +6,14 @@
     Array               ["Me", "John", "Collin", "Sam"]
     Object              {Name: "Me", Location:"Here"}
     Object              {Name: "Me", Location:"Here", favoriteShows: []}
+
+    const colors = ["red", "yellow", "white", "blue", "purple", "black"];
+    let(i = 0; i < colors.length; i++) {
+        if (colors[i] === "blue") {
+            console.log(i);
+        };
+    };
+
+    const tommie = {favFood: 'Ice Cream', hobby: 'chess', liveIn: 'Northglenn', favDataType: 'string'};
+    console.log(`Tommie's hobby is ${tommie.hobby}!`);
+
