89dc1ddf6765b7e9096c205bbb4612b95706b6a1
Tommie <tommiealcorn@gmail.com>
2019-03-28 07:27:29 -0600

    data types

---
+++ js/app.js
@@ -0,0 +1,8 @@
+/*
+
+    Boolean             (1 or 0), (true or false)
+    String              "Value"
+    Data Structure      {"hull", "Laser", true, 4}
+    Array               ["Me", "John", "Collin", "Sam"]
+    Object              {Name: "Me", Location:"Here"}
+    Object              {Name: "Me", Location:"Here", favoriteShows: []}
